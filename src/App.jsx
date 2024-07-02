3cd1f1de1e2e1e82cfb92ab637b3ad30c46c073e
rehman <rehmanafzal942@gmail.com>
2024-07-02 11:17:50 -0700

    initial React setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Landing from './Desktop/Landing/Landing.jsx';
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='landing-wraper'>
+      <Landing/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Desktop/Landing/Landing.jsx
@@ -0,0 +1,10 @@
+import './landing.css';
+
+export default function Landing () {
+    return <div className="landing-main">
+        This is Home Page
+        <div className="hello">
+        Hello
+        </div>
+    </div>
+}
